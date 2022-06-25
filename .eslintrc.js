a7d13000746475f3fd6ef91f8c8a8caa0de69e58
ryota-murakami <dojce1048@gmail.com>
2022-06-26 04:36:22 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+/**
+ * @type {import('@types/eslint').Linter.BaseConfig}
+ */
+module.exports = {
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "@remix-run/eslint-config/jest-testing-library",
+    "prettier",
+  ],
+  rules: {
+    "prettier/prettier": [
+      "warn",
+      {},
+      {
+        "properties": {
+          "usePrettierrc": true
+        }
+      }
+    ],
+  },
+  // we're using vitest which has a very similar API to jest
+  // (so the linting plugins work nicely), but it means we have to explicitly
+  // set the jest version.
+  settings: {
+    jest: {
+      version: 27,
+    },
+  },
+};
