5cb744ea655a5be23499f0132d9ed53f56a1cbe7
ryota-murakami <dojce1048@gmail.com>
2022-06-26 12:38:29 +0900

    lets dymamic routing

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,17 +1,10 @@
+import { prisma } from "~/db.server";
+
 export type Post = {
   slug: string;
   title: string;
 };
 
-export async function getPosts(): Promise<Array<Post>> {
-  return [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
+export async function getPosts() {
+  return prisma.post.findMany();
 }
