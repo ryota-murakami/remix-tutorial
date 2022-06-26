d77c4d4fd48c52b53d5e623ecda1d3a2ff2dd639
ryota-murakami <dojce1048@gmail.com>
2022-06-26 10:49:33 +0900

    finish server ppart

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,37 +1,27 @@
 import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
+import type { Post } from "~/models/post.server";
 
-type Post = {
-  slug: string;
-  title: string;
-};
+import { getPosts } from "~/models/post.server";
 
 type LoaderData = {
-  posts: Array<Post>;
+  // this is a handy way to say: "posts is whatever type getPosts resolves to"
+  posts: Awaited<ReturnType<typeof getPosts>>;
 };
 
 export const loader = async () => {
   return json<LoaderData>({
-    posts: [
-      {
-        slug: "my-first-post",
-        title: "My First Post",
-      },
-      {
-        slug: "90s-mixtape",
-        title: "A Mixtape I Made Just For You",
-      },
-    ],
+    posts: await getPosts(),
   });
 };
-// ...
+
 export default function Posts() {
   const { posts } = useLoaderData() as LoaderData;
   return (
     <main>
       <h1>Posts</h1>
       <ul>
-        {posts.map((post) => (
+        {posts.map((post: Post) => (
           <li key={post.slug}>
             <Link to={post.slug} className="text-blue-600 underline">
               {post.title}
